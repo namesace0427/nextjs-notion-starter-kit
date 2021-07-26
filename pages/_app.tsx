8365d5c0ce0fcbce91416b7e8262a604cd24f130
Zealot <jaws02144@naver.com>
2021-07-26 17:36:07 +0900

    add custom css
    custom css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,6 +23,8 @@ import 'react-static-tweets/styles.css'
 // global style overrides for notion
 import 'styles/notion.css'
 
+import 'styles/custom.css'
+
 // global style overrides for prism theme (optional)
 import 'styles/prism-theme.css'
 
